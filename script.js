fa5d5e6fb3cf93a5c431e6c76a36c2da41616cfc
Joshua504 <chukwuebukajosh8@gmail.com>
2023-11-17 01:53:51 +0100

    made the popup display onclick then when value is inputed it displays the content

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-const checkBox = document.querySelectorAll(".checkbox");
-const editMenu = document.querySelector(".edit-menu");
+const showPopup = document.querySelector(".show-popup");
+const exitPopup = document.querySelector(".exit");
+const popup = document.querySelector(".popup");
 
-checkBox.addEventListener("click", () => {
-  if (checkBox.checked) {
-    editMenu.style.display = "block";
-  } else {
-    editMenu.style.display = "none";
+showPopup.addEventListener("click", () => {
+  popup.classList.add("display-pop");
+});
+exitPopup.addEventListener("click", () => {
+  popup.classList.remove("display-pop");
+});
+
+const inputEl = document.querySelector(".input");
+const addTaskEl = document.querySelector(".add-task");
+const contentBox = document.querySelector(".content-box");
+
+const currentTime = new Date();
+
+const hours = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+
+const valueContainer = [];
+
+addTaskEl.addEventListener("click", () => {
+  const value = inputEl.value;
+  valueContainer.push(value);
+
+  const contentItem = ` 
+<div class="content-item">
+<div>
+  <input class="checkbox" type="checkbox" />
+</div>
+<div class="text"><p>${value}</p></div>
+<div><p>${hours} : ${minutes}</p></div>
+<select id="dropdown">
+  <option value="option1">active</option>
+  <option value="option2">progress</option>
+  <option value="option3">done</option>
+</select>
+<div class="progress-bar">
+  <div class="progress"></div>
+</div>
+</div>
+`;
+
+  if(value === ''){
+    popup.classList.remove("display-pop");
+  }else{
+    contentBox.innerHTML += contentItem;
+    inputEl.value = ''
+    popup.classList.remove("display-pop");
   }
 });
